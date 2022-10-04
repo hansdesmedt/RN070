f73cc0495e2ce3b6c4081d90af2c0f6ce61c9768
Hans De Smedt <hans.desmedt@gmail.com>
2022-10-04 16:27:59 +0200

    added react-native-gradients, try interpolateColor reanimated

--- App.tsx
+++ App.tsx
@@ -1,120 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React from 'react';
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import Animated, {
+  Easing,
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {RadialGradient} from 'react-native-gradients';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const progress = useSharedValue<number>(0);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // const config = useInterpolateConfig(
+  //   [0, 2],
+  //   ['#000000', '#FFFFFF'],
+  //   ColorSpace.RGB,
+  // );
+
+  // progress.value = withRepeat(
+  //   withTiming(0, {
+  //     duration: 500,
+  //     easing: Easing.out(Easing.exp),
+  //   }),
+  //   -1,
+  //   true,
+  // );
+
+  // const backgroundStyle = useAnimatedStyle(() => {
+  //   const color = interpolateColor(progress.value, [0, 1], ['red', 'blue']);
+
+  //   console.log('backgroundColorInterpolation', color);
+
+  //   return {
+  //     backgroundColor: color,
+  //   };
+  // });
+
+  const colorList = [
+    {offset: '0%', color: '#231557', opacity: '1'},
+    {offset: '29%', color: '#44107A', opacity: '1'},
+    {offset: '67%', color: '#FF1361', opacity: '1'},
+    {offset: '100%', color: '#FFF800', opacity: '1'},
+  ];
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <RadialGradient x="50%" y="50%" rx="50%" ry="50%" colorList={colorList} />
   );
+  // return <LinearGradient colorList={colorList} angle={90} />;
+  // return <Animated.View style={[{flex: 1}, backgroundStyle]} />;
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
